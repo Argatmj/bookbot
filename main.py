bc02cf9117e5d1f66f5b00feb07402bf5975e3bf
Argatmj <ikomangarga0612@gmail.com>
2024-11-12 19:55:11 +0100

    Improved count words function and changed layout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 def count_words(text):
-    word_count = 0
-    words = text.split()
-    for word in words:
-        word_count +=1
-    return word_count
+    return len(text.split())
 
 def count_characters(text):
     char_count = {}
@@ -24,17 +20,20 @@ def sort_on(char_dict):
 def print_report(word_count, char_list,book):
     char_list.sort(reverse=True,key=sort_on)
     print(f"--- Begin report of {book} ---")
-    print(f"{word_count} words found in the document")
-    print("\n")
+    print(f"{word_count} words found in the document\n")
     for char_dict in char_list:
         for char in char_dict:
             if char.isalpha():
                 print(f"The '{char}' was found {char_dict[char]} times")
     print("--- End report ---")
 
-book = "books/frankenstein.txt"
-with open(book) as f:
-    file_contents = f.read()
-count = count_words(file_contents)
-char_list = count_characters(file_contents)
-print_report(count,char_list,book)
+def main():
+    book = "books/frankenstein.txt"
+    with open(book) as f:
+        file_contents = f.read()
+    count = count_words(file_contents)
+    char_list = count_characters(file_contents)
+    print_report(count,char_list,book)
+
+if __name__ == "__main__":
+    main()
